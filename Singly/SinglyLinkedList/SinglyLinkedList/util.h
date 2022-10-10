25e8550598614d1f274523402f2d2ff1c06c9ce2
LSW9609-PC00\USER <lsw9609@smartradarsystem.com>
2022-10-10 23:16:59 +0900

    refresh some dir

---
+++ Singly/SinglyLinkedList/SinglyLinkedList/util.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef UTIL_H
+#define UTIL_H
+typedef struct node
+{
+    struct NODE* next;
+    int data;
+}NODE;
+NODE* createNode(int num);
+void appendNode(NODE* target, int data);
+void removeNode(NODE* target, int data);
+void printAllList(NODE* curr);
+void application(int lenth);
+void freeAllList(NODE* curr);
+#endif // !UTIL_H
